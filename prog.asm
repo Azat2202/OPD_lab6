a65ef1fc02db068a55820c635f64f88259f5737a
Azat2202 <azat222@gmail.com>
2023-06-05 00:19:08 +0300

    feat: added interrupts on VU2 and VU3

--- prog.asm
+++ prog.asm
@@ -1,19 +1,36 @@
 			ORG		0x0
-v0:			WORD	$default, 0x180
-v1:			WORD	$default, 0x180
-v2:			WORD	$default, 0x180
-v3:			WORD	$default, 0x180
-v4:			WORD	$default, 0x180
-v5:			WORD	$default, 0x180
-v6:			WORD	$default, 0x180
-v7:			WORD	$default, 0x180
+v0:			WORD	$default, 	0x180
+v1:			WORD	$default, 	0x180
+v2:			WORD	$int2, 		0x180
+v3:			WORD	$int3, 		0x180
+v4:			WORD	$default, 	0x180
+v5:			WORD	$default, 	0x180
+v6:			WORD	$default, 	0x180
+v7:			WORD	$default, 	0x180
 
 default:	IRET	
+
+int2:		DI
+			IN		0x4
+			NEG				;Проверка на ОДЗ не требуется потому что после расширения знака выхода за диапазон не произойдет
+			ST		$X
+			NOP
+			IRET
+
+int3:		DI
+			LD		$X
+			NOP				;Отладочная метка
+			ASL
+			ADD		$X		;Вычисление f(x) = 3x+6
+			ADD		#0x6
+			OUT		0x6
+			EI
+			IRET
 
 			ORG		0x039
 X:			WORD	0xF0F0
 X_MAX:		WORD	0x7FFF
-X_MIN:		WORD	0x8003	;Нижняя граница ОДЗ (включительно)
+X_MIN:		WORD	0x8003	;Нижняя граница ОДЗ (включительно) 100000000
 
 START:		DI
 			CLA
@@ -31,17 +48,5 @@ MAIN:		DI				;Запрет прерываний для атомарности о
 			LD		X_MAX	
 ODZ_OK:		ST		$X		
 			EI
-			OUT		0x1
 			BR		MAIN
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 			
